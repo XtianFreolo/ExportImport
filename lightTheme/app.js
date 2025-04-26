f7e7027bebcaa54165f7fb63f19a012308940f2a
XtianFreolo <f60@umbc.edu>
2025-04-26 03:52:30 -0400

    added light and dark theme

---
+++ lightTheme/app.js
@@ -0,0 +1,16 @@
+
+
+
+async function loadConfig() {
+    const hour = new Date().getHours();
+    const theme = await import('./theme.mjs');
+  
+    if (hour < 18) {
+      theme.setLightTheme();
+    } else {
+      theme.setDarkTheme();
+    }
+  }
+  
+  loadConfig();
+  
---
+++ lightTheme/theme.js
@@ -0,0 +1,10 @@
+
+
+export function setLightTheme() {
+    console.log("Light theme has been set.");
+  }
+  
+  export function setDarkTheme() {
+    console.log("Dark theme has been set.");
+  }
+  
